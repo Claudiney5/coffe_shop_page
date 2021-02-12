0b2140c7a3e635475bc160e1d328fd0688882465
Claudiney5 <claudiney.mail@gmail.com>
2021-02-12 17:36:01 -0300

    model products

--- kaliSite/admin.py
+++ kaliSite/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 
-from .models import Product, Client
+from .models import Product, Client, Order
 
 # registra na aplicação os modelos das Aplicações.
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('nome', 'preco', 'estoque')
 
+class ClientAdmin(admin.ModelAdmin):
+    list_client = ('nome', 'sobrenome', 'endereco', 'cidade', 'email', 'fone' )
+
+class OrderAdmin(admin.ModelAdmin):
+    list_order = ('cliente', 'produto', 'quantidade', 'data', 'email', 'fone' )
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Client)
+admin.site.register(Order)
--- kaliSite/models.py
+++ kaliSite/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Product(models.Model):
     nome = models.CharField('Nome', max_length=100)
-    preco = models.DecimalField('Preço', decimal_places=2, max_digits=7)
+    preco = models.DecimalField('Preço', blank=True, decimal_places=2, max_digits=7)
     estoque = models.IntegerField('Qtdade em Estoque', default=0)
 
     def __str__(self):
@@ -14,11 +14,22 @@ class Product(models.Model):
 class Client(models.Model):
     nome = models.CharField('Nome', max_length=20)
     sobrenome = models.CharField('Sobrenome', max_length=50)
-    endereco= models.CharField('Endereço', max_length=30)
+    endereco= models.CharField('Endereço', max_length=100)
     cidade = models.CharField('Cidade', max_length=40)
     estado = models.CharField('Estado', max_length=30)
     email = models.EmailField('Email', max_length=80)
-    phone = models.IntegerField('Telefone')
+    fone = models.IntegerField('Telefone')
 
     def __str__(self):
         return f'{self.nome} {self.sobrenome}'
+
+class Order(models.Model):
+    cliente = models.CharField('Cliente', max_length=100)
+    produto = models.CharField('Produto', max_length=100)
+    quantidade = models.IntegerField('Quantidade')
+    data = models.DateField('Data')
+    email = models.EmailField('Email', max_length=80)
+    fone = models.IntegerField('Telefone')
+
+    def __str__(self):
+        return f'{self.cliente} - {self.quantidade} de {self.produto}'
